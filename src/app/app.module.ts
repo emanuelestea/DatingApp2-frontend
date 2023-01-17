56d197086f868557bf948e0b4fe86f8dd8627dfc
emanuelestea <emanuele.stea@outlook.it>
2023-01-17 09:54:23 +0100

    end of section 12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component'
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 //dropdown from angular bootstrap
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
@@ -28,6 +28,8 @@ import { JwtInterceptor } from './interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { LoadingInterceptor } from './interceptors/loading.interceptor';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import { TextInputComponent } from './_forms/text-input/text-input.component';
+import { DatePickerComponent } from './_forms/date-picker/date-picker.component';
 
 @NgModule({
   declarations: [
@@ -44,7 +46,9 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     ServerErrorComponent,
     MemberCardComponent,
     MemberEditComponent,
-    PhotoEditorComponent
+    PhotoEditorComponent,
+    TextInputComponent,
+    DatePickerComponent,
   ],
   imports: [
     BrowserModule,
@@ -52,7 +56,8 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -12,27 +14,61 @@ export class RegisterComponent implements OnInit {
 
   //@Input() usersFromParent: any;
   @Output() sendback = new EventEmitter();
-
-  model: any = {};
+  registerForm: FormGroup = new FormGroup({});
+  maxDate: Date = new Date();
+  validationError: string[] | undefined;
 
   constructor(
     private accountService: AccountService,
-    private toast: ToastrService  
+    private toast: ToastrService,
+    private fb: FormBuilder,
+    private roouter: Router,
   ) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+  }
+
+  initializeForm() {
+    //this.registerForm = new FormGroup({
+    //  username: new FormControl('', Validators.required),
+    //  password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+    //  confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')]),
+    //});
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]],
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    })
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : {notMatching: true}
+    }
   }
 
   register() {
-    this.accountService.register(this.model).subscribe(
-      response => {
-        console.log(response);
-        this.cancel();
+    const dob = this.getDateOnly(this.registerForm.controls['dateOfBirth'].value);
+    const value = {...this.registerForm.value, dateOfBirth: dob};
+    this.accountService.register(value).subscribe({
+      next: () => {
+        this.roouter.navigateByUrl('/members')
       },
-      error => {
-        this.toast.error(error.error);
+      error: error => {
+        this.validationError = error;
       }
-    )
+      
+    })
     
   }
 
@@ -40,4 +76,10 @@ export class RegisterComponent implements OnInit {
     this.sendback.emit(false);
   }
 
+  private getDateOnly (dob: string | undefined) {
+    if (!dob) return;
+    let theDob = new Date(dob);
+    return new Date(theDob.setMinutes(theDob.getMinutes()-theDob.getTimezoneOffset()))
+      .toISOString().slice(0,10);
+  }
 }
